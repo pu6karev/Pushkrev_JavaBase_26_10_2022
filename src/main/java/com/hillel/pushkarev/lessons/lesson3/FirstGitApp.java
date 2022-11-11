be3a27acd7a2206c1e6e94553b500106ad4861f9
Pushkarev Vasiliy <pu6karev@gmail.com>
2022-11-11 20:11:30 +0200

    add file for lesson3

---
+++ src/main/java/com/hillel/pushkarev/lessons/lesson3/FirstGitApp.java
@@ -0,0 +1,8 @@
+package com.hillel.pushkarev.lessons.lesson3;
+
+public class FirstGitApp {
+    public static void main(String[] args) {
+
+        System.out.println("First commit and push to github");
+    }
+}
