966a7f62383baf0e22fc88a31117e2b26d9a3e79
Pushkarev Vasiliy <pu6karev@gmail.com>
2022-11-11 20:22:36 +0200

    add new print line

--- src/main/java/com/hillel/pushkarev/lessons/lesson3/FirstGitApp.java
+++ src/main/java/com/hillel/pushkarev/lessons/lesson3/FirstGitApp.java
@@ -4,5 +4,9 @@ public class FirstGitApp {
     public static void main(String[] args) {
 
         System.out.println("First commit and push to github");
+
+        //int i_var = 40;
+        
+        System.out.println("New line for changing");
     }
 }
