58b3eea1655605a83fdef9301cbc5f91e3d9903e
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-01-18 03:09:19 +0200

    add test classes

--- src/main/java/com/hillel/pushkarev/lessons/ArrayUtil.java
+++ src/main/java/com/hillel/pushkarev/lessons/ArrayUtil.java
@@ -1,16 +1,56 @@
 package com.hillel.pushkarev.lessons;
 
+
+
+
+
 public class ArrayUtil {
 
-    int[] arrayBasket = {5, 10, 12, 18, 36};
+    // --- метод № 1 - для подсчета среднего значения элементов массива
+    // если передан пустой массив вернуть -1
+    // если передан null вернуть -2
 
-    public double arrayAvg(int[] array){
+    public static double arrayAvg(int[] array){
+
+        if(array == null){
+            return -2;
+        }
+
+        if(array.length == 0){
+            return -1;
+        }
+
         double sum = 0;
 
         for (int elem: array) {
-            sum += (double) elem;
+            sum += elem;
         }
 
         return (sum / array.length);
+    }
+
+
+    // *****************************************
+    // --- метод № 2 - для проверки массива на квадратность
+    // если передан пустой массив вернуть -1
+    // если передан null вернуть -2
+    // если массив квадратный вернуть 1
+    // в остальных случаях вернуть 0
+
+    public static int arrayCheckSquare(int[][] array2){
+
+        if(array2 == null){
+            return -2;
+        }
+
+        if(array2.length == 0){
+            return -1;
+        }
+
+        if(array2.length == array2[0].length){
+            return 1;
+        }
+
+        return 0;
     }
 }
--- src/test/java/com/hillel/pushkarev/lessons/homework18/TestArrayUtil.java
+++ src/test/java/com/hillel/pushkarev/lessons/homework18/TestArrayUtil.java
@@ -1,11 +1,49 @@
 package com.hillel.pushkarev.lessons.homework18;
 
+import com.hillel.pushkarev.lessons.ArrayUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class TestArrayUtil {
 
+    // --- метод № 1 - для подсчета среднего значения элементов массива
+    // если передан пустой массив вернуть -1
+    // если передан null вернуть -2
     @Test
     public void test1(){
-
+        int[] array = new int[0];
+        Assertions.assertEquals(-1, ArrayUtil.arrayAvg(array));
     }
+
+    @Test
+    public void test2(){
+        Assertions.assertEquals(-2, ArrayUtil.arrayAvg(null));
+    }
+
+    // *****************************************
+    // --- метод № 2 - для проверки массива на квадратность
+    // если передан пустой массив вернуть -1
+    // если передан null вернуть -2
+    // если массив квадратный вернуть 1
+    // в остальных случаях вернуть 0
+
+    @Test
+    public void test3(){
+        int[][] array = new int[0][];
+        Assertions.assertEquals(-1, ArrayUtil.arrayCheckSquare(array));
+    }
+
+    @Test
+    public void test4(){
+        int[][] array = null;
+        Assertions.assertEquals(-2, ArrayUtil.arrayCheckSquare(array));
+    }
+
+    @Test
+    public void test5(){
+        int[][] array = new int[5][5];
+        Assertions.assertEquals(1, ArrayUtil.arrayCheckSquare(array));
+    }
+
 }
