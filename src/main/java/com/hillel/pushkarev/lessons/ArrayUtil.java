8ff7443c309ed1758a2eeedbca293ec83bccd54c
Pushkarev Vasiliy <pu6karev@gmail.com>
2023-01-18 10:59:31 +0200

    add test classes fix array equals

--- src/main/java/com/hillel/pushkarev/lessons/ArrayUtil.java
+++ src/main/java/com/hillel/pushkarev/lessons/ArrayUtil.java
@@ -34,8 +34,8 @@ public class ArrayUtil {
     // --- метод № 2 - для проверки массива на квадратность
     // если передан пустой массив вернуть -1
     // если передан null вернуть -2
+    // если массив не квадратный вернуть -3
     // если массив квадратный вернуть 1
-    // в остальных случаях вернуть 0
 
     public static int arrayCheckSquare(int[][] array2){
 
@@ -47,10 +47,16 @@ public class ArrayUtil {
             return -1;
         }
 
-        if(array2.length == array2[0].length){
-            return 1;
+
+        // если размеру 1-й строки не соотв. размер хотя бы одной из колонок, массив - не квадрат
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                if(array2.length != array2[0].length){
+                    return -3;
+                }
+            }
         }
 
-        return 0;
+        return 1;
     }
 }
